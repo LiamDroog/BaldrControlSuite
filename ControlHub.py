e28370f55972cee04b115a9356b9261e41d165c6
Liam <droog@ualberta.ca>
2021-06-02 14:41:15 -0600

    Reworked parts to comply to a more object oriented structure Added a diagnostic readout and worked on a way to add them in gui But a ballin folder system in the gui

--- ControlHub.py
+++ ControlHub.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import font, messagebox
 from droogCNC import TwoAxisStage
 from HDF5Browser import FileBrowser
@@ -6,6 +7,8 @@ import HDF5Methods as h5m
 import serial.tools.list_ports
 from ImageViewer import ImageFrame
 from QueueClass import Queue
+from DiagnosticAddingClass import AddDiagnosticFrame
+import h5py
 from PIL import Image, ImageTk
 import numpy as np
 import pickle
@@ -16,7 +19,7 @@ import os
 import cv2
 from datetime import date
 from FLIRLib import BFSCam
-
+from FLIRTrigger import BFSTrigger
 import PySpin
 
 class ControlHub:
@@ -35,6 +38,34 @@ class ControlHub:
         self.colarr = list(i for i in range(self.grid[0]))
         self.window.rowconfigure(self.rowarr, minsize=25, weight=1)
         self.window.columnconfigure(self.colarr, minsize=25, weight=1)
+
+        self.tabcontrol = ttk.Notebook(self.window)
+
+        self.control_tab = ttk.Frame(self.tabcontrol)
+        self.diag_tab = ttk.Frame(self.tabcontrol)
+        self.diag_config = ttk.Frame(self.tabcontrol)
+
+        self.control_tab.rowconfigure(self.rowarr, minsize=25, weight=1)
+        self.control_tab.columnconfigure(self.colarr, minsize=25, weight=1)
+
+        self.diag_row_arr = list(i for i in range(3))
+        self.diag_tab.rowconfigure(self.diag_row_arr, minsize=1, weight=1)
+        self.diag_tab.columnconfigure(self.diag_row_arr, minsize=1, weight=1)
+
+        self.tabcontrol.add(self.control_tab, text='Control')
+        self.tabcontrol.add(self.diag_tab, text='Configure Diagnostics')
+        self.tabcontrol.add(self.diag_config, text='Add / Remove Diagnostics')
+        self.tabcontrol.grid(row=1, column=0, rowspan=self.grid[1]-1, columnspan=self.grid[0], sticky='news')
+
+        self.diag_config.rowconfigure([1], minsize=25, weight=1)
+        self.diag_config.columnconfigure([1], minsize=25, weight=1)
+        self.Diagnostics_list = []
+
+        for i in range(len(self.diag_row_arr)):
+            for j in range(len(self.diag_row_arr)):
+                self.Diagnostics_list.append(DiagnosticFrame(self.diag_tab, i, j))
+
+        self.diagnosticEditor = AddDiagnosticFrame(self.diag_config, len(self.Diagnostics_list))
         self.shotnum = 0
         self.shotfile = None
         self.imageframe = None
@@ -47,11 +78,12 @@ class ControlHub:
         self.filename = None
         self.stage = None
         self.tempFile = None
-
+        self.stageparameters = {}
         # migrate to class?
         self.system = PySpin.System.GetInstance()
 
-        #self.bfs = BFSCam(self.system.GetCameras()[0])
+        #self.bfs = BFSTrigger()
+        self.bfs = None
 
         # title
         self.title_frame = tk.Frame(master=self.window, relief='raised', borderwidth=2)
@@ -61,10 +93,10 @@ class ControlHub:
         self.title_label.pack()
 
         # ################ LAUNCH FRAME ################
-        self.launch_frame = tk.Frame(master=self.window, relief='groove', borderwidth=3)
+        self.launch_frame = tk.Frame(master=self.control_tab, relief='groove', borderwidth=3)
         self.launch_frame.grid(row=1, column=0, rowspan=9, columnspan=3, sticky='nsew')
         self.stagelabel = tk.Label(master=self.launch_frame, text='Stage Control')
-        self.stagelabel.grid(row=0, column=0, columnspan=3, sticky='ew' )
+        self.stagelabel.grid(row=0, column=0, columnspan=3, sticky='ew')
         # stage gui launch btn
         self.start_stage_btn = tk.Button(master=self.launch_frame, text='Launch Stage Control',
                                          command=self.__launchStageControl)
@@ -72,7 +104,7 @@ class ControlHub:
 
         # hdf5 storage file
         today = date.today()
-        self.h5filepath = 'datafiles/'+today.strftime("%b-%d-%Y")
+        self.h5filepath = 'datafiles/' + today.strftime("%b-%d-%Y")
 
         if self.__createdir(self.h5filepath):
             print('dir made')
@@ -105,7 +137,7 @@ class ControlHub:
 
         self.h5header = tk.Label(master=self.launch_frame, text='HDF5 Files')
         self.h5header.grid(row=5, column=0, columnspan=3, sticky='ew')
-        self.h5filetxt = tk.Label(master=self.launch_frame, text='File: '+str(self.shotfile))
+        self.h5filetxt = tk.Label(master=self.launch_frame, text='File: ' + str(self.shotfile))
         self.h5filetxt.grid(row=6, column=0, columnspan=3)
 
         # file browser gui button
@@ -122,34 +154,32 @@ class ControlHub:
         self.runprogrambutton['state'] = tk.DISABLED
         self.killButton = tk.Button(master=self.launch_frame, text='KILL', command=self.__killSwitch)
 
-        self.programfiletext.grid(row=8, column=0, columnspan=3,  sticky='ew')
+        self.programfiletext.grid(row=8, column=0, columnspan=3, sticky='ew')
         self.programfileinput.grid(row=9, column=0, rowspan=1, columnspan=3, sticky='ew')
         self.getprogramfilebutton.grid(row=9, column=3, sticky='ew')
         self.runprogrambutton.grid(row=9, column=4, sticky='ew')
-
 
         # shoot the laser button
         self.lasertext = tk.Label(master=self.launch_frame, text='Laser Controls')
         self.lasertext.grid(row=10, column=0, columnspan=3, sticky='ew')
         self.armLaser_btn = tk.Button(master=self.launch_frame, text='Arm Laser', fg='#C02f1d',
-                                        command=self.__armLaser)
+                                      command=self.__armLaser)
         self.armLaser_btn.grid(column=0, columnspan=2, row=11, sticky='nsew')
 
         self.shootLaser_btn = tk.Button(master=self.launch_frame, text='Fire Laser', bg='#BEBEBE')
         self.shootLaser_btn.grid(column=0, columnspan=2, row=12, sticky='nsew')
+        self.shootLaser_btn['state'] = tk.DISABLED
 
         self.revive_button = tk.Button(master=self.launch_frame)
 
-
-
         # ################ READOUT FRAME ################
-        self.readout_frame = tk.Frame(master=self.window, relief='groove', borderwidth=3)
+        self.readout_frame = tk.Frame(master=self.control_tab, relief='groove', borderwidth=3)
         self.readout_frame.grid(row=1, column=4, rowspan=9, columnspan=9, sticky='nsew')
         self.readout_frame.rowconfigure(list(i for i in range(16)), weight=1)
         self.readout_frame.columnconfigure(list(i for i in range(9)), weight=1)
 
-        self.get_spectra_btn = tk.Button(master=self.readout_frame, text='Get shot spectra',
-                                         command=self.__captureimage)
+        self.get_spectra_btn = tk.Button(master=self.readout_frame, text='Get last image',
+                                         command=self.__getCurrentImage)
         self.get_spectra_btn.grid(row=1, column=0, columnspan=3, sticky='new')
 
         self.stage_pos = tk.StringVar()
@@ -164,9 +194,18 @@ class ControlHub:
         self.cam_lbl['font'] = font.Font(size=14)
         self.camera1.set('Camera Not Connected')
 
+        if self.bfs:
+            self.camera1.set('Camera Connected')
 
+        self.window.geometry(
+            '800x600+%d+%d' % (self.window.winfo_screenwidth() / 4, self.window.winfo_screenheight() / 5))
         self.window.protocol("WM_DELETE_WINDOW", self.__on_closing)
         self.__setTempFile()
+        self.window.update()
+        # now root.geometry() returns valid size/placement
+        self.window.minsize(self.window.winfo_width(), self.window.winfo_height())
+
+
 
     def start(self):
         """
@@ -176,22 +215,26 @@ class ControlHub:
         """
         self.window.mainloop()
 
+
     def __on_closing(self):
         # if messagebox.askokcancel("Quit", "Quit? This will close all windows."):
-            #self.window.destroy()
-        if self.bfs:
-            self.bfs.deInit()
+        # self.window.destroy()
         self.window.destroy()
 
     def __armLaser(self):
-        if not self.laserArmed:
-            self.armLaser_btn.config(bg='#228C22', fg='#FFFFFF')
-            self.shootLaser_btn.config(command=self.__fireLaser, bg='#c02f1d', fg='#FFFFFF')
-            self.laserArmed = True
-        else:
-            self.armLaser_btn.config(bg='#F0F0F0', fg='red')
-            self.shootLaser_btn.config(command=tk.NONE, bg='#BEBEBE', fg='#000000')
-            self.laserArmed = False
+        if self.stage != None:
+            if not self.laserArmed:
+                self.shootLaser_btn['state'] = tk.ACTIVE
+                self.armLaser_btn.config(bg='#228C22', fg='#FFFFFF')
+                self.shootLaser_btn.config(command=self.__fireLaser, bg='#c02f1d', fg='#FFFFFF')
+                self.laserArmed = True
+                self.bfs.init()
+            else:
+                self.armLaser_btn.config(bg='#F0F0F0', fg='red')
+                self.shootLaser_btn['state'] = tk.DISABLED
+                self.shootLaser_btn.config(command=tk.NONE, bg='#BEBEBE', fg='#000000')
+                self.laserArmed = False
+                self.bfs.deinit()
 
     def __fireLaser(self):
         """
@@ -203,10 +246,30 @@ class ControlHub:
         data being collected.
         :return: None
         """
-        self.__createShotFile()
-        # fire
+        if self.stage.isOpen():
+            self.__createShotFile()
+            image = self.__captureimage()
+            # self.__triggerLaser()
+            # self.__takePicture() for all cameras?? Multithreaded might be needed here for simultaneity
+            self.__saveData(image)
 
-        # collect data
+    def __saveData(self, image):
+        # stage:
+        st = time.time()
+        owd = os.getcwd()
+        try:
+            os.chdir(self.h5filepath)
+            h5m.createDataset(self.shotfile, 'StagePos', data=self.stage.getPos())
+            for key, val in self.stage.getStageParameters().items():
+                h5m.setMetadata(self.shotfile, str(key), str(val), path='StagePos')
+            h5m.createDataset(self.shotfile, 'BFSimage', data=image)
+            h5m.setMetadataFromDict(self.shotfile, self.bfs.getDeviceParams(), path='BFSimage')
+        except Exception as e:
+            print('Error occured saving data')
+            print(e)
+        finally:
+            os.chdir(owd)
+            print('saving data took ' + str(time.time() - st) + ' seconds')
 
     def __launchStageControl(self):
         """
@@ -249,6 +312,7 @@ class ControlHub:
 
         if not self.filebrowser:
             self.filebrowser = FileBrowser()
+            self.filebrowser.parseCommand('cd ' + self.h5filepath)
 
     def __createdir(self, name):
         try:
@@ -292,29 +356,24 @@ class ControlHub:
             h5m.createFile(self.shotfile)
             self.h5filetxt['text'] = 'File: ' + self.shotfile
         except Exception as e:
-            print('Error occured creating shot file:' + e)
+            print('Error occured creating shot file:', e)
 
         finally:
             os.chdir(owd)
 
     def __captureimage(self):
-        image = self.bfs.getImage()
+        return self.bfs.run_single_camera(self.bfs.cam)
 
-        imgname = 'images/spectra_shot_00' + str(self.shotnum) + '.png'
-        cv2.imwrite(imgname, image.GetNDArray())
-        self.shotnum += 1
-        print(self.imageframerunning)
-
-        if not self.imageframerunning:
-            self.imageframerunning = True
-            self.imageframe = ImageFrame('Image', imgname)
-        else:
-            print('added')
-            self.imageframe.addImage(imgname)
-
-    def __getspectra(self):
-        # this will get the plot of most recent shot spectra
-        pass
+    def __getCurrentImage(self):
+        owd = os.getcwd()
+        try:
+            os.chdir(self.h5filepath)
+            image = h5m.getData(self.shotfile, 'BFSimage')
+            self.imageframe = ImageFrame('', image=image)
+        except Exception as e:
+            print(e)
+        finally:
+            os.chdir(owd)
 
     def __getRunFile(self, filename):
         try:
@@ -336,7 +395,8 @@ class ControlHub:
             self.getprogramfilebutton.config(bg='red', command=tk.NONE)
             self.window.after(1000,
                               lambda: self.getprogramfilebutton.config(bg='#F0F0F0',
-                                                                             command=lambda: self.__getRunFile(self.programfileinput.get())))
+                                                                       command=lambda: self.__getRunFile(
+                                                                           self.programfileinput.get())))
         else:
             self.__showKillButton()
 
@@ -344,23 +404,12 @@ class ControlHub:
         if not self.temp_save_running:
             self.temp_save_running = True
             self.__saveTempData()
-
-        next = self.queue.dequeue()
-        self.__sendCommand(next)
-
-        if self.queue.size() > 0:
-            self.stage.sendCommand(next)
-            self.window.after(50, self.__runProgram)
-        #     self.window.after(self.calcDelay(self.currentpos, next), self.__runProgram)
-        #     self.currentpos = next
-        #
-        elif self.queue.size() == 0:
-            return
-        #     self.window.after(self.calcDelay(self.currentpos, next), self.finishRun)
-        #
-        # else:
-        #     self.temprunning = False
-        #     return
+        if not self.queue.is_empty():
+            next = self.queue.dequeue()
+            self.__sendCommand(next)
+        else:
+            self.stage.sendOutput('Run Finished')
+            self.__finishRun()
 
     def __showKillButton(self):
         self.killButton.grid(row=9, column=5, sticky='ew')
@@ -368,7 +417,31 @@ class ControlHub:
         self.runprogrambutton.config(command=self.__runProgram)
 
     def __sendCommand(self, command):
-        print(command)
+        if command.strip().lower() != 'fire':
+            if self.queue.size() >= 0:
+                self.stage.sendCommand(command)
+                self.window.after(1000, self.__runProgram)
+            #     self.window.after(self.calcDelay(self.currentpos, next), self.__runProgram)
+            #     self.currentpos = next
+            #
+            # elif self.queue.size() == 0:
+            #     self.__finishRun()
+            # else:
+            #     self.temp_save_running = False
+            #     return
+        else:
+            self.__fireLaser()
+            self.window.after(1000, self.__runProgram)
+
+    def __finishRun(self):
+        """
+        removes temp file at end of program run
+
+        :return: None
+        """
+        print('finish run called')
+        os.remove(self.tempFile)
+        self.tempFile = None
 
     def __killSwitch(self):
         if self.queue:
@@ -393,7 +466,8 @@ class ControlHub:
     def __saveTempData(self):
         """
         Saves temp data to temp file, if program is currently running it calls itself every 1000ms
-
+        # todo: implement different method for finding lost position - right now if there
+                are duplicates it will break ://
         :return: None
         """
         if self.temp_save_running:
@@ -409,15 +483,6 @@ class ControlHub:
         currentline, t, self.filename, self.shotnum = np.load(self.tempFile, allow_pickle=True)
         print(currentline, t, self.filename)
         return currentline
-
-    def __removeTempFile(self):
-        """
-        removes temp file at end of program run
-
-        :return: None
-        """
-        os.remove(self.tempFile)
-        self.tempFile = None
 
     def __revive(self):
         """
@@ -477,3 +542,68 @@ class ControlHub:
                 self.window.after(delay, lambda: self.__blinkButton(button, c1, c2, delay))
         except:
             return
+
+    def __calculateStageDelay_ms(self, currentpos, nextpos):
+        ipos = self.__parsePosition(currentpos)
+        fpos = self.__parsePosition(nextpos)
+        assert len(ipos) == len(fpos), 'Input arrays must be same length'
+
+        v = float(self.stage.getStageParameters()['xMaxRate'][1]) / 60
+        a = float(self.stage.getStageParameters()['xMaxRate'][1])  # becomes very choppy changing to xMaxAcc... idk wtf
+
+        delta = list(ipos[i] - fpos[i] for i in range(len(ipos)))
+        d = np.sqrt(sum(i ** 2 for i in delta))
+        deltaT = ((2 * v) / a) + ((d - (v ** 2 / a)) / v)
+        print('next move in ' + str(int(np.floor(deltaT * 1000))) + 'ms')
+        return int(np.floor(deltaT * 1000))  # in ms
+
+    def __parsePosition(self, ipos):
+        """
+        Parses position for use with DRO
+
+        :param ipos: input position
+        :return: [x, y] list of current position
+        """
+        pos = [0, 0]
+        for i in ipos.split(' '):
+            if i.lower()[0] == 'x':
+                pos[0] = float(i[1:])
+            if i.lower()[0] == 'y':
+                pos[1] = float(i[1:])
+        return pos
+
+
+class DiagnosticFrame:
+    def __init__(self, master, posx, posy):
+        self.master = master
+        self.xpos = posx
+        self.ypos = posy
+        self.subframe = tk.Frame(master=self.master, relief='raised', borderwidth=2)
+        self.subframe.rowconfigure(list(i for i in range(4)), minsize=1, weight=1)
+        self.subframe.columnconfigure(list(i for i in range(2)), minsize=1, weight=1)
+        self.subframe.grid(row=self.xpos, column=self.ypos, sticky='nsew')
+
+        self.name = tk.Label(master=self.subframe, text='Enter Diagnostic Name')
+        self.name.grid(row=0, column=0)
+        self.name_entry = tk.Entry(master=self.subframe)
+        self.name_entry.grid(row=0, column=1, sticky='ew')
+
+        self.dir_label = tk.Label(master=self.subframe, text='Enter Directory')
+        self.dir_label.grid(row=1, column=0)
+        self.dir_entry = tk.Entry(master=self.subframe)
+        self.dir_entry.grid(row=1, column=1, sticky='ew')
+
+        self.enabled = tk.IntVar()
+        self.enabledButton = tk.Checkbutton(master=self.subframe, text='Enable Diagnostic', variable=self.enabled)
+        self.enabledButton.grid(row=2, column=0, columnspan=2)
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    console = ControlHub()
+    console.start()
+
