11d12b6e41e077902d2c577b4804e2417065407a
Liam <droog@ualberta.ca>
2021-06-07 14:06:53 -0600

    Added docstrings

--- DataParsing.py
+++ DataParsing.py
@@ -4,12 +4,23 @@ import os
 
 
 class DataDaemon:
+    """
+    Instatiates a watchdog to poll input directory for files and moves them to target directory. Multiple can be
+    instantiated (one per diagnostic) so long as they read from different directories. I suggest directories based off
+    of the diagnostic name or serial number.
+    """
     def __init__(self, watchdir, movedir, interval):
+        """
+
+        :param watchdir: Directory to watch for new files
+        :param targetdir: Directory to write files to
+        :param interval: Time interval between calls
+        """
         cwd = os.path.dirname(__file__)
         os.chdir(cwd)
         self.p = multiprocessing.Process(target=pollFiles, args=(watchdir, movedir, interval))
 
-    def main(self):
+    def start(self):
         self.p.start()
 
     def kill(self):
