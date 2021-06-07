1cd21ad9249164ebea3fba2f3e395cd7a34d26c7
Liam <droog@ualberta.ca>
2021-06-07 14:07:15 -0600

    Added Docstrings

--- DiagnosticAddingClass.py
+++ DiagnosticAddingClass.py
@@ -4,25 +4,29 @@ import os
 import numpy as np
 
 class AddDiagnosticFrame:
-    """savefile = {
-        1: {
-            param1: name
-            param2: date
-            param3: weather
-        },
-        2: {
-            param1: name
-            param2: date
-            param3: weather
-        },
-        3: {
-            param1: name
-            param2: date
-            param3: weather
-        },
-    }
     """
-    diagnosticsFile = 'Data/DiagnosticConfig.npy'
+        Class to facilitate the addition and removal of diagnostic systems within the control hub.
+
+        diagnosticfile format = {
+            1: {
+                param1: name
+                param2: date
+                param3: weather
+            },
+            2: {
+                param1: name
+                param2: date
+                param3: weather
+            },
+            3: {
+                param1: name
+                param2: date
+                param3: weather
+            }, et cetera...
+        }
+    """
+    diagnosticsFile = 'Config/DiagnosticConfig.npy'
+
     def __init__(self, master, num):
         self.master = master
         self.diag_num = num
@@ -36,6 +40,7 @@ class AddDiagnosticFrame:
         self.FLIRsubframe.columnconfigure(list(i for i in range(16)), minsize=1, weight=1)
         self.FLIRsubframe.grid(row=0, column=0, rowspan=9, columnspan=16, sticky='nsew')
 
+        # list of availible diagnostics
         self.dlist = [i+1 for i in range(num)]
         self.FLIRselected_diagnostic = tk.IntVar()
         self.FLIRselected_diagnostic.set(1)
@@ -109,18 +114,21 @@ class AddDiagnosticFrame:
         self.folders.grid(row=0, column=0, rowspan=9, columnspan=16, sticky='nsew')
 
     def __setFLIRParams(self):
-
+        # check if diagnostics file exists
         if os.path.exists(self.diagnosticsFile):
             diagnostics = np.load(self.diagnosticsFile, allow_pickle='True')
             if len(diagnostics.item().keys()) != 0 and self.FLIRselected_diagnostic.get() in diagnostics.item().keys():
-                # handle overwrite prompt
+                # todo: handle overwrite prompt
                 self.addFlirButton.config(text='Diagnostic already exists')
                 self.master.after(2000, lambda: self.addFlirButton.config(text='Apply Changes'))
                 return
         else:
+            # if it doesnt exits, create and save an empty dictionary to it
             tempdict = {}
             np.save(self.diagnosticsFile, tempdict)
         # TODO: parse for correctness, not just emptiness. PathExists, etc
+        #   Also, *type* of data (image, pos, spectra, etc)
+        # ensure all boxes have input in them - doesnt check correctness
         if self.cameranameentry.get().strip() == '':
             self.cameraname.config(text='Enter Camera Name', fg='red')
             self.master.after(3000, lambda: self.cameraname.config(text='Name:', fg='black'))
@@ -137,11 +145,13 @@ class AddDiagnosticFrame:
             self.flirfilepath.config(text='Must be valid directory', fg='red')
             self.master.after(3000, lambda: self.flirfilepath.config(text='Data File Path:', fg='black'))
 
+        # add pairs to dictionary from input boxes
         self.FLIRparams['Diagnostic Name'] = self.cameranameentry.get()
         self.FLIRparams['Serial Number'] = self.serialnumberentry.get()
         self.FLIRparams['File Path'] = self.flirfilepathentry.get()
         self.FLIRparams['Enabled On Startup'] = self.FLIRstartenable.isEnabled()
 
+        # save to diagnostics file
         diagdict = np.load(self.diagnosticsFile, allow_pickle='True').item()
         diagdict[int(self.FLIRselected_diagnostic.get())] = self.FLIRparams
         np.save(self.diagnosticsFile, diagdict)
@@ -150,6 +160,7 @@ class AddDiagnosticFrame:
         return
 
     def __removeDiagnostic(self, num):
+        # remove specific diag from file
         d = np.load(self.diagnosticsFile, allow_pickle=True).item()
         d.pop(num)
         np.save(self.diagnosticsFile, d)
